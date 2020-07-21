f410b9ce2c08265fa274f5d2bbd28ffb3214a15e
zaid450 <me@zaid-arshad.tech>
2020-07-21 17:24:20 -0400

    support for multiple punishments, placeholder for other options

--- src/Internals/Voice/Stream.js
+++ src/Internals/Voice/Stream.js
@@ -9,6 +9,7 @@ class Stream {
 	}
 	async init() {
 		let threshold = (await this.connection.channel.guild.threshold) === "high" ? 2000 : (await this.connection.channel.guild.threshold) === "medium" ? 1750 : 1500;
+		let punishment = await this.connection.channel.guild.punishment;
 		let count = 5;
 		let muted = false; 
 		this.stream.on("data", async (data) => {
@@ -16,13 +17,36 @@ class Stream {
 			else count = 5;
 			if (muted) return;
 			if (Stream.getVolume(data) > threshold) {
-				muted = true;
-				await this.member.voice.setMute(true);
-				this.member.send("You have been muted for `10 Seconds` due to excessive volume coming from your mic. If you did not do this intentionally, please take proper measures to ensure this doesn't happen again.").catch(() => {});
-				setTimeout(async () => {
-					await this.member.voice.setMute(false);
-					muted = false;
-				}, 10000);
+				switch (punishment) {
+				case "vc_mute": {
+					muted = true;
+					await this.member.voice.setMute(true);
+					break;
+				}
+				case "vc_kick": {
+					await this.member.voice.setChannel(null);
+					break;
+				}
+				/*
+				case "g_kick": {
+					await this.member.kick("Kicked for exceeding threshold.");
+					break;
+				}
+				case "g_ban": {
+					await this.member.ban("Banned for exceeding threshold.");
+					break;
+				}
+				case "g_mute": {
+					break;
+				}*/
+				}
+				this.member.send(`You have been ${punishment === "vc_mute" ? "`muted for 10 seconds`" : punishment === "vc_kick" ? "`kicked from the voice channel`" : "unknown type"} due to excessive volume coming from your mic. If you did not do this intentionally, please take proper measures to ensure this doesn't happen again. Further punishment may occur if you don't comply.`).catch(() => {});
+				if(punishment === "vc_mute") {
+					setTimeout(async () => {
+						await this.member.voice.setMute(false);
+						muted = false;
+					}, 10000);
+				}
 			}
 		});
 		return this;
